10711f33aaa913122ffeca23d07514ee19f476df
rohitdhum <rohit45dhumal@gmail.com>
2025-11-25 11:10:21 +0530

    Add files via upload

---
+++ PureVirtualDemo1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class Base
+{
+    public:
+     int i,j;
+     int Addition(int a, int b)  //Concrete
+     {
+        return a+b; 
+     }
+
+     virtual int Substraction(int a, int b) = 0  //Abstract   
+};    
+
+#pragma pack(1)
+class Derived : public Base  //Error
+{
+    public:
+     int x;
+
+    
+};
+
+int main()
+{  
+    Base *bp = new Derived();     //up casting
+
+    
+    return 0;
+}
---
+++ PureVirtualDemo2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class Base
+{
+    public:
+     int i,j;
+     int Addition(int a, int b)  //Concrete
+     {
+        return a+b; 
+     }
+
+     virtual int Substraction(int a, int b) = 0;  //Abstract
+
+};  
+
+#pragma pack(1)
+class Derived : public Base  //Error
+{
+    public:
+     int x;
+
+     int Substraction(int a, int b)   //Concrete
+     {
+        return a-b;
+     } 
+
+      int Multiplication(int a, int b)   //Concrete
+     {
+        return a*b;
+     }
+};    
+
+int main()
+{  
+    Base *bp = new Derived();     //up casting
+    int iRet = 0;
+
+    iRet = bp->Addition(11,10);       //21
+    cout<<iRet<<"\n";
+
+    iRet = bp->Substraction(11,10);   //1
+    cout<<iRet<<"\n";
+
+   // iRet = bp->Multiplication(11,10);  //Error  
+   
+
+    return 0;
+}
