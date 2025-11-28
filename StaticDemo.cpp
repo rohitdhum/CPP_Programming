eb01ff1ccd5ea52a3045b3ce0ce5c1cc313c0085
rohitdhum <rohit45dhumal@gmail.com>
2025-11-28 19:27:00 +0530

    Add files via upload

---
+++ StaticDemo.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+       int i;
+       int j;
+       static int x;
+
+       Demo()
+       {
+          this->i = 0;
+          this->j = 0;
+       }
+
+       Demo(int a, int b)
+       {
+        this->i = a;
+        this->j = b;
+       }
+       void fun()
+       {
+        cout<<"Inside Fun\n";
+       }
+
+       static void Gun()
+       {
+         cout<<"Inside Gun\n";
+       }
+
+};
+
+int Demo :: x = 11;
+
+int main()
+{
+    Demo::Gun();
+    cout<<"value of x is :"<<Demo::x<<"\n";
+
+    return 0;
+}
